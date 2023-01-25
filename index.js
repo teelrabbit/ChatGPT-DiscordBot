9bbeccb3c61730ef87c734d9213f2fb196df6dfa
John Reich <55339121+teelrabbit@users.noreply.github.com>
2023-01-24 23:34:47 -0800

    added some basic functions for interaction between chatgdp and the client

---
+++ index.js
@@ -0,0 +1,31 @@
+//Create a discord bot using OpenAI that interacts on the Discord server
+require('dotenv').config();
+
+//Prepare to connect to the Discord API
+
+const { Client, GatewayIntentBits } require('discord.js');
+const client = new Client({ intents: [
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMessages,
+  GatewayIntentBits.MessageContent
+]})
+
+//Prepare connection to OpenAI API
+
+const { Configuration , OpenAIApi } = require ('openai');
+const configuration = new Configuration({
+  organization: process.env.OPENAI_ORG,
+  apiKey: process.env.OPENAI_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+//Check for when a message on discord is sent
+client.on('messageCreate', async function(message)){
+  try (
+    console.log(message.content);
+    message.reply('You said: ${message.content}');
+  ) catch(err){
+    console.log(err)
+  }
+});
