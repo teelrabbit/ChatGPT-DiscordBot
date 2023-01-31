b7052ccf071fa9faaa44006c2cdbc2fe267d7221
FBueller <55339121+teelrabbit@users.noreply.github.com>
2023-01-30 17:45:27 -0800

    Update index.js
    Making major changes to index.js for deployment to ebs

--- index.js
+++ index.js
@@ -1,59 +1,38 @@
-// using enviorment variables in aws 
-const AWS = require('aws-sdk');
-
-// Create a new SSM client
-const ssm = new AWS.SSM();
-
-async function getParameter(parameterName) {
-    try {
-        const result = await ssm.getParameter({
-            Name: parameterName,
-            WithDecryption: true
-        }).promise();
-        return result.Parameter.Value;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-const discordToken = getParameter('DISCORD_TOKEN');
+// referncing enviorment varibles
+require('dotenv').config();
+const OPENAI_KEY = process.env.OPENAI_KEY;
+const OPENAI_ORG = process.env.OPENAI_ORG;
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 //Create a discord bot using OpenAI that interacts on the Discord server
 require('dotenv').config();
-
 //Prepare to connect to the Discord API
-
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [
   GatewayIntentBits.Guilds,
   GatewayIntentBits.GuildMessages,
   GatewayIntentBits.MessageContent
 ]})
-
 //Prepare connection to OpenAI API
-
 const { Configuration , OpenAIApi } = require ('openai');
 const configuration = new Configuration({
   organization: process.env.OPENAI_ORG,
   apiKey: process.env.OPENAI_KEY,
 });
-
 const openai = new OpenAIApi(configuration);
-
 //Check for when a message on discord is sent
 client.on('messageCreate', async function(message){
   try {
     console.log(message.content);
     if(message.author.bot) return; // generate a response of exactly what i said in chat
     //message.reply(`Greeting Bueller: ${message.content}`)
-
     const gdpResponse = await openai.createCompletion({
       model: "text-davinci-002",
       prompt: `ChatGDP is a coding helper.\n\
       HAL: Hello, I am ChatGDP. I am here to help you with your coding problems. What is the problem you are facing today? \n\
       ${message.author.username}: ${message.content}\n\
       HAL:`,
-      temperature: 0.5,
+      temperature: 0.1,
       max_tokens: 3000,
       stop: ["HAL:", "F.Bueller:"],
     })
@@ -64,7 +43,6 @@ client.on('messageCreate', async function(message){
     console.log(err)
   }
 });
-
 //Log the bot onto discord
-client.login(process.env.discordToken)
+client.login(process.env.DISCORD_TOKEN)
 console.log("ChatGDP Bot is online on Discord")
