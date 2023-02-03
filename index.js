7e922afa8e8a95f1823b469c53d7eba6ff2b8aee
John Reich <55339121+teelrabbit@users.noreply.github.com>
2023-02-02 18:55:33 -0800

    making changes to intial check

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 // referncing enviorment varibles
-require('dotenv').config();
-/*
-var OPENAI_KEY = process.env.api_key;
-var OPENAI_ORG = process.env.org_key;
-var DISCORD_TOKEN = process.env.discord_key;
-*/
+
 //check for env vars
-console.log(process.env.DISCORD_TOKEN);
-if (!DISCORD_TOKEN) {
-  console.error("Discord token not set.");
-  console.log(DISCORD_TOKEN)
-  process.exit(1);
+//console.log(process.env.discord_key);
+if (process.env.discord_key) {
+console.error("Discord token is set");
+//console.log(process.env.discord_key);
+//console.log(process.env.org_key);
+process.exit(0);
 }
+//gdp suggestion
 //Create a discord bot using OpenAI that interacts on the Discord server
-require('dotenv').config();
 //Prepare to connect to the Discord API
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [
@@ -24,8 +20,8 @@ const client = new Client({ intents: [
 //Prepare connection to OpenAI API
 const { Configuration , OpenAIApi } = require ('openai');
 const configuration = new Configuration({
-  organization: process.env.OPENAI_ORG,
-  apiKey: process.env.OPENAI_KEY,
+  organization: process.env.org_key,
+  apiKey: process.env.api_key,
 });
 const openai = new OpenAIApi(configuration);
 //Check for when a message on discord is sent
@@ -36,8 +32,8 @@ client.on('messageCreate', async function(message){
     //message.reply(`Greeting Bueller: ${message.content}`)
     const gdpResponse = await openai.createCompletion({
       model: "text-davinci-002",
-      prompt: `ChatGDP is a coding helper.\n\
-      HAL: Hello, I am ChatGDP. I am here to help you with your coding problems. What is the problem you are facing today? \n\
+      prompt: `I am Elon Musk.\n\
+      HAL: Hello, I am Elon Musk. I am here to help explain things to you. What is the problem you are facing today? \n\
       ${message.author.username}: ${message.content}\n\
       HAL:`,
       temperature: 0.1,
@@ -52,5 +48,5 @@ client.on('messageCreate', async function(message){
   }
 });
 //Log the bot onto discord
-client.login(DISCORD_TOKEN)
+client.login(process.env.discord_key)
 console.log("ChatGDP Bot is online on Discord")
