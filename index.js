7cdab8b56cebb9dd44a9265c60447d4b1f15c81e
FBueller <55339121+teelrabbit@users.noreply.github.com>
2023-02-13 18:51:46 -0800

    adding code to estanlish connection to api (#87)

--- index.js
+++ index.js
@@ -2,49 +2,57 @@
 
 //check for env vars
 if (!process.env.discord_key) {
-console.error("Discord token read as undefined.");
-process.exit(0);
-}
-//gdp suggestion
-//Create a discord bot using OpenAI that interacts on the Discord server
-//Prepare to connect to the Discord API
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [
-  GatewayIntentBits.Guilds,
-  GatewayIntentBits.GuildMessages,
-  GatewayIntentBits.MessageContent
-]})
-//add code for some sort of database
-
-//Check for when a message on discord is sent
-client.on('messageCreate', async function(message){
-  try {
-    console.log(message.content);
-    if(message.author.bot) return; // generate a response of exactly what i said in chat
-    //message.reply(`Greeting Bueller: ${message.content}`)
-    const gdpResponse = await openai.createCompletion({
-      model: "text-davinci-002",
-      prompt: `I am Elon Musk. I am here to answer your code questions. What would you like to know? \n\
-      ${message.author.username}: ${message.content}\n\
-      HAL:`,
-      temperature: 0.1,
-      max_tokens: 4000,
-      stop: ["HAL:", "F.Bueller:"],
-    })
-
-    message.reply(`${gdpResponse.data.choices[0].text}`);
-    return;
-  } catch(err){
-    console.log(err)
+  console.error("Discord token read as undefined.");
+  process.exit(0);
   }
-});
-//Log the bot onto discord
-client.login(process.env.discord_key)
-.then(() => {
-    console.log("ChatGDP Bot is online on Discord")
-})
-.catch((error) => {
-    console.error("Failed to log in to Discord:", error)
-})
-
-
+  //gdp suggestion
+  //Create a discord bot using OpenAI that interacts on the Discord server
+  //Prepare to connect to the Discord API
+  const { Client, GatewayIntentBits } = require('discord.js');
+  const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]})
+  //Prepare connection to OpenAI API
+  const { Configuration , OpenAIApi } = require ('openai');
+  const configuration = new Configuration({
+    organization: process.env.org_key,
+    apiKey: process.env.api_key,
+  });
+  const openai = new OpenAIApi(configuration);
+  //add other code later for storing feature 
+  
+  //Check for when a message on discord is sent
+  client.on('messageCreate', async function(message){
+    try {
+      console.log(message.content);
+      if(message.author.bot) return; // generate a response of exactly what i said in chat
+      //message.reply(`Greeting Bueller: ${message.content}`)
+      const gdpResponse = await openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: `I am Elon Musk. I am here to answer your code questions. What would you like to know? \n\
+        ${message.author.username}: ${message.content}\n\
+        HAL:`,
+        temperature: 0.1,
+        max_tokens: 4000,
+        stop: ["HAL:", "F.Bueller:"],
+      })
+  
+      message.reply(`${gdpResponse.data.choices[0].text}`);
+      return;
+    } catch(err){
+      console.log(err)
+    }
+  });
+  //Log the bot onto discord
+  client.login(process.env.discord_key)
+  .then(() => {
+      console.log("ChatGDP Bot is online on Discord")
+  })
+  .catch((error) => {
+      console.error("Failed to log in to Discord:", error)
+  })
+  
+  
+  
