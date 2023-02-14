fb1399d2c9a1de587ceb323e91fc7f5a9e28ccd5
John Reich <55339121+teelrabbit@users.noreply.github.com>
2023-02-13 19:38:09 -0800

    only have the dependency added

--- index.js
+++ index.js
@@ -24,33 +24,14 @@ if (!process.env.discord_key) {
   //Create connection to SQL Database
   const mysql = require('mysql2');
 
-  const connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    port: process.env.RDS_PORT
-  });  
+  //const connection = mysql.createConnection({
+    //host: process.env.RDS_HOSTNAME,
+    //user: process.env.RDS_USERNAME,
+    //password: process.env.RDS_PASSWORD,
+    //database: process.env.RDS_DB_NAME,
+    //port: process.env.RDS_PORT
+  //});  
 
-// Connect to the database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to database: ' + err.stack);
-    return;
-  }
-  console.log('Connected to database as id ' + connection.threadId);
-});
-
-// Handle errors that occur during the connection
-connection.on('error', (err) => {
-  console.error('Database error: ' + err);
-  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-    connection.connect();
-  } else {
-    throw err;
-  }
-});
-  
   //Check for when a message on discord is sent
   client.on('messageCreate', async function(message){
     try {
