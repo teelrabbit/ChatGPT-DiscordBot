146bb08c662f5a56e22e2e26c847ac4401690fa5
FBueller <55339121+teelrabbit@users.noreply.github.com>
2023-02-15 19:02:47 -0800

    refactoring code (#101)

--- index.js
+++ index.js
@@ -1,81 +1,88 @@
-// referncing enviorment varibles
-
-//check for env vars
-if (!process.env.discord_key) {
-  console.error("Discord token read as undefined.");
+// referencing environment variables
+// check for env vars
+if (!process.env.discord_key || !process.env.org_key || !process.env.api_key) {
+  console.error("One or more environment variables are missing.");
   process.exit(0);
-  }
-  //gdp suggestion
-  //Create a discord bot using OpenAI that interacts on the Discord server
-  //Prepare to connect to the Discord API
-  const { Client, GatewayIntentBits } = require('discord.js');
-  const client = new Client({ intents: [
+}
+
+// Prepare to connect to the Discord API
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent
-  ]})
-  //Prepare connection to OpenAI API
-  const { Configuration , OpenAIApi } = require ('openai');
-  const configuration = new Configuration({
-    organization: process.env.org_key,
-    apiKey: process.env.api_key,
-  });
-  const openai = new OpenAIApi(configuration);
-  //Create connection to SQL Database
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// Prepare connection to OpenAI API
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  organization: process.env.org_key,
+  apiKey: process.env.api_key,
+});
+const openai = new OpenAIApi(configuration);
+
+// Create connection to SQL Database
 /*
-  const mysql = require('mysql2');
+const mysql = require('mysql2');
 
-  const connection = mysql.createConnection({
-    host: process.env.RDS_HOSTNAME,
-    user: process.env.RDS_USERNAME,
-    password: process.env.RDS_PASSWORD,
-    database: process.env.RDS_DB_NAME,
-    port: process.env.RDS_PORT
-  });  
-  */
- // Creating a data structure to store responses 
- const conversationHistory = {};
+const connection = mysql.createConnection({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DB_NAME,
+  port: process.env.RDS_PORT
+});  
+*/
 
-  //Check for when a message on discord is sent
-  client.on('messageCreate', async function(message){
-    try {
-      console.log(message.content);
-      if(message.author.bot) return; // generate a response of exactly what i said in chat
-      //message.reply(`Greeting Bueller: ${message.content}`)
+// Create a data structure to store previous prompts and responses
+const chatHistory = {};
 
-      // check for if response to prompt was saved 
-      if (message.content in conversationHistory) {
-        message.reply(conversationHistory[message.content]);
-        return;
-      }
+// Check for when a message on Discord is sent
+client.on("messageCreate", async function (message) {
+  try {
+    console.log(message.content);
+    if (message.author.bot) return;
 
-      const gdpResponse = await openai.createCompletion({
-        model: "text-davinci-002",
-        prompt: `I am Elon Musk. I am here to answer your code questions. What would you like to know? \n\
-        ${message.author.username}: ${message.content}\n\
-        HAL:`,
-        temperature: 0.1,
-        max_tokens: 4000,
-        stop: ["HAL:", "F.Bueller:"],
-      })
-     
-      //save the prompt and response to data struct
-      conversationHistory[message.content] = gdpResponse.data.choices[0].text;
-      
-      message.reply(`${gdpResponse.data.choices[0].text}`);
+    // Check if we have a previous response for this prompt
+    const prompt = `${message.author.username}: ${message.content}`;
+    const previousResponse = chatHistory[prompt];
+
+    // If we have a previous response, use it directly
+    if (previousResponse) {
+      message.reply(previousResponse);
       return;
-    } catch(err){
-      console.log(err)
     }
-  });
-  //Log the bot onto discord
-  client.login(process.env.discord_key)
+
+    // Otherwise, generate a new response using OpenAI
+    const gdpResponse = await openai.createCompletion({
+      model: "text-davinci-002",
+      prompt: `I am Elon Musk. I am here to answer your code questions. What would you like to know? \n\
+        ${prompt}\n\
+        HAL:`,
+      temperature: 0.1,
+      max_tokens: 4000,
+      stop: ["HAL:", "F.Bueller:"],
+    });
+
+    const response = gdpResponse.data.choices[0].text;
+
+    // Save the new response in the chat history
+    chatHistory[prompt] = response;
+
+    message.reply(response);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Log the bot onto Discord
+client
+  .login(process.env.discord_key)
   .then(() => {
-      console.log("ChatGDP Bot is online on Discord")
+    console.log("ChatGDP Bot is online on Discord");
   })
   .catch((error) => {
-      console.error("Failed to log in to Discord:", error)
-  })
-  
-  
-  
+    console.error("Failed to log in to Discord:", error);
+  });
