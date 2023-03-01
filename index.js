0f0f2750e1c731306d5eff454224e8b8f096a20b
John Reich <55339121+teelrabbit@users.noreply.github.com>
2023-02-28 21:27:32 -0800

    testing adding dynamo db with error feedback

--- index.js
+++ index.js
@@ -22,6 +22,30 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+//changes here ---------------------------------------------------------------
+// Load the AWS SDK for JavaScript
+const AWS = require("aws-sdk");
+
+// Configure the SDK with your AWS credentials
+AWS.config.update({
+  region: "us-west-1",
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.aws_secret_access_key,
+});
+
+// Create a DynamoDB client object
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+// Test the connection to DynamoDB
+docClient.listTables((err, data) => {
+  if (err) {
+    console.error("Failed to connect to DynamoDB:", err);
+    process.exit(1);
+  } else {
+    console.log("Connected to DynamoDB successfully");
+  }
+});
+
 // Object to store chat history
 const chatHistory = {};
 
