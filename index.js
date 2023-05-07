bfe60a95d92c9002b3e3874f554aafc5fb3c189b
FBueller <55339121+teelrabbit@users.noreply.github.com>
2023-05-07 13:10:10 -0700

    Revert "reverting dyamodb changes (#130)" (#131)
    This reverts commit 249100f1610c0f9680fb005763bc971bed14fd01.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// check for env vars
 if (!process.env.discord_key || !process.env.org_key || !process.env.api_key) {
   console.error("One or more environment variables are missing.");
   process.exit(0);
 }
 
+// Prepare to connect to the Discord API
 const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
   intents: [
@@ -12,6 +14,7 @@ const client = new Client({
   ],
 });
 
+// Prepare connection to OpenAI API
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   organization: process.env.org_key,
@@ -19,15 +22,44 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+//changes here ---------------------------------------------------------------
+// Load the AWS SDK for JavaScript
+console.log('AWS-SDK Prepapring to instakll');
+const AWS = require("aws-sdk");
+
+// Configure the SDK with your AWS credentials
+AWS.config.update({
+  region: "us-west-1",
+  accessKeyId: process.env.aws_access_key_id,
+  secretAccessKey: process.env.aws_secret_access_key,
+});
+
+// Test the connection to DynamoDB
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = "GPT-Response-DB";
+console.log('connection prepared!');
+
+// Object to store chat history
+const chatHistory = {};
+
+// Check for when a message on Discord is sent
 client.on("messageCreate", async function (message) {
   try {
     console.log(message.content);
     if (message.author.bot) return;
 
+    // Get the prompt from the message content
     const prompt = message.content;
 
+    // Check if there is a cached response for the prompt
+    if (chatHistory[prompt]) {
+      message.reply(chatHistory[prompt]);
+      return;
+    }
+
+    // Otherwise, generate a new response using OpenAI
     const gdpResponse = await openai.createCompletion({
-      model: "text-davinci-002",
+      model: "gpt-3.5-turbo",
       prompt: `I am Elon Musk. I am here to answer your code questions. What would you like to know? \n\
         ${prompt}\n\
         H10:`,
@@ -38,13 +70,17 @@ client.on("messageCreate", async function (message) {
 
     const response = gdpResponse.data.choices[0].text;
 
+    // Save the new response in the chat history
+    chatHistory[prompt] = response;
+
     message.reply(response);
   } catch (err) {
     console.log(err);
     message.reply("There was an error processing your request.");
   }
 });
-
+console.log('testenv');
+// Log the bot onto Discord
 client
   .login(process.env.discord_key)
   .then(() => {
